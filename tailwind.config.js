8be777e726954e1e8437c7db10ed36576bd44490
Jan Leigh Muñoz <janleigh@proton.me>
2026-05-08 07:51:02 +0000

    refactor: redo proect structure

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ["./src/app/**/*.{js,jsx,ts,tsx}", "./src/components/**/*.{js,jsx,ts,tsx}"],
+	content: [
+		"./src/app/*.{ts,tsx}",
+		"./src/app/**/*.{js,jsx,ts,tsx}",
+		"./src/components/**/*.{js,jsx,ts,tsx}",
+	],
 	presets: [require("nativewind/preset")],
 	theme: {
 		extend: {},
